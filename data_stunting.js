c7ff35d8d040cd0b6aa1eeb01addcbddcaf8ab3f
yudono <youdhono@gmail.com>
2023-07-04 22:55:02 +0700

    update new data

---
+++ data_stunting.js
@@ -0,0 +1,173 @@
+// sumber : https://databoks.katadata.co.id/datapublish/2023/02/02/daftar-prevalensi-balita-stunting-di-indonesia-pada-2022-provinsi-mana-teratas
+let data_stunting = [
+  {
+    tahun: 2022,
+    provinsi: "Nusa Tenggara Timur",
+    persentase: 35.3,
+  },
+  {
+    tahun: 2022,
+    provinsi: "Sulawesi Barat",
+    persentase: 35,
+  },
+  {
+    tahun: 2022,
+    provinsi: "Papua",
+    persentase: 34.6,
+  },
+  {
+    tahun: 2022,
+    provinsi: "Nusa Tenggara Barat",
+    persentase: 32.7,
+  },
+  {
+    tahun: 2022,
+    provinsi: "Aceh",
+    persentase: 31.2,
+  },
+  {
+    tahun: 2022,
+    provinsi: "Papua Barat",
+    persentase: 30,
+  },
+  {
+    tahun: 2022,
+    provinsi: "Sulawesi Tengah",
+    persentase: 28.2,
+  },
+  {
+    tahun: 2022,
+    provinsi: "Kalimantan Barat",
+    persentase: 27.8,
+  },
+  {
+    tahun: 2022,
+    provinsi: "Sulawesi Tenggara",
+    persentase: 27.7,
+  },
+  {
+    tahun: 2022,
+    provinsi: "Sulawesi Selatan",
+    persentase: 27.2,
+  },
+  {
+    tahun: 2022,
+    provinsi: "Kalimantan Tengah",
+    persentase: 26.9,
+  },
+  {
+    tahun: 2022,
+    provinsi: "Maluku Utara",
+    persentase: 26.1,
+  },
+  {
+    tahun: 2022,
+    provinsi: "Maluku",
+    persentase: 26.1,
+  },
+  {
+    tahun: 2022,
+    provinsi: "Sumatera Barat",
+    persentase: 25.2,
+  },
+  {
+    tahun: 2022,
+    provinsi: "Kalimantan Selatan",
+    persentase: 24.6,
+  },
+  {
+    tahun: 2022,
+    provinsi: "Kalimantan Timur",
+    persentase: 23.9,
+  },
+  {
+    tahun: 2022,
+    provinsi: "Gorontalo",
+    persentase: 23.8,
+  },
+  {
+    tahun: 2022,
+    provinsi: "Kalimantan Utara",
+    persentase: 22.1,
+  },
+  {
+    tahun: 2022,
+    provinsi: "Sumatera Utara",
+    persentase: 21.1,
+  },
+  {
+    tahun: 2022,
+    provinsi: "Jawa Tengah",
+    persentase: 20.8,
+  },
+  {
+    tahun: 2022,
+    provinsi: "Sulawesi Utara",
+    persentase: 20.5,
+  },
+  {
+    tahun: 2022,
+    provinsi: "Jawa Barat",
+    persentase: 20.2,
+  },
+  {
+    tahun: 2022,
+    provinsi: "Banten",
+    persentase: 20,
+  },
+  {
+    tahun: 2022,
+    provinsi: "Bengkulu",
+    persentase: 19.8,
+  },
+  {
+    tahun: 2022,
+    provinsi: "Jawa Timur",
+    persentase: 19.2,
+  },
+  {
+    tahun: 2022,
+    provinsi: "Sumatera Selatan",
+    persentase: 18.6,
+  },
+  {
+    tahun: 2022,
+    provinsi: "Kep Bangka Belitung",
+    persentase: 18.5,
+  },
+  {
+    tahun: 2022,
+    provinsi: "Jambi",
+    persentase: 18,
+  },
+  {
+    tahun: 2022,
+    provinsi: "Riau",
+    persentase: 17,
+  },
+  {
+    tahun: 2022,
+    provinsi: "DI Yogyakarta",
+    persentase: 16.4,
+  },
+  {
+    tahun: 2022,
+    provinsi: "Kepulauan Riau",
+    persentase: 15.4,
+  },
+  {
+    tahun: 2022,
+    provinsi: "Lampung",
+    persentase: 15.2,
+  },
+  {
+    tahun: 2022,
+    provinsi: "DKI Jakarta",
+    persentase: 14.8,
+  },
+  {
+    tahun: 2022,
+    provinsi: "Bali",
+    persentase: 8,
+  },
+];
